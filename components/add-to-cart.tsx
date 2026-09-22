d5b94f2701ed4f56ca7632829bf884fb93020db4
rihygodapo <wowaci8528@kierko.com>
2026-09-22 12:35:42 +0500

    Polish storefront UI and branding

--- components/add-to-cart.tsx
+++ components/add-to-cart.tsx
@@ -1,1 +1,1 @@
-"use client"; import type {Product} from "@/lib/products"; import {useCart} from "./cart-provider"; export function AddToCart({product}:{product:Product}){const{addItem}=useCart();return <button className="button" onClick={()=>addItem(product)}>Add to cart</button>}
+"use client"; import type {Product} from "@/lib/products"; import {useCart} from "./cart-provider"; export function AddToCart({product}:{product:Product}){const{addItem}=useCart();return <button className="button add" onClick={()=>addItem(product)}>Add to bag <span>+</span></button>}
